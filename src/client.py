3af05f8f2016d0e0746cf8498eaee72ffa5a96a2
Konstantin Anisimov <anisimovkosta19@gmail.com>
2026-04-13 21:09:18 +0300

    Added the restrictions to the client

--- src/client.py
+++ src/client.py
@@ -1,4 +1,4 @@
 from langchain_ollama import ChatOllama
 from .config import OLLAMA_BASE_URL, OLLAMA_MODEL
 
-llm_client = ChatOllama(base_url=OLLAMA_BASE_URL, model=OLLAMA_MODEL)
+llm_client = ChatOllama(base_url=OLLAMA_BASE_URL, model=OLLAMA_MODEL, num_ctx=4096, num_predict=256)
